75f354c38583755d456ede8029dc7d1bd0ba871b
enahoroenas <orieroenahoro@yahoo.com>
2022-11-06 14:33:42 -0800

    added projects

---
+++ models/Project.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const ProjectSchema = new mongoose.Schema({    
+    topic:{
+        type: String,
+        default:'',
+        required:true,
+        unique:true
+    },
+    completed:{
+        type: String,
+        default:'0',
+    }, 
+    unit:{
+        type: String,
+        default:'',
+    }, 
+    total:{
+        type: String,
+        default:'',
+        required:true
+    }, 
+    createdBy:{
+        type: mongoose.Types.ObjectId,
+        required: [true,'please provide User'],
+        ref:'User'
+    }
+},{timestamps:true}
+)
+
+export default mongoose.model('Project',ProjectSchema)
