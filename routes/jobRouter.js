c0f3fa6a0ce85a0232f73e8ad8683c5033dc76dd
enahoroenas <orieroenahoro@yahoo.com>
2022-11-08 01:51:29 -0800

    Added events

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {createComment,sendEmail,addImage,getAllImages,updateComment,deleteComment,addLeader,getAllExco,
     addNews,getNews,getAllMembers,deleteNews,getComments,addProject,getProject,editproject,deleteProject,
+    addEvent,getEvent,deleteEvent,
 } from '../controllers/jobsController.js'
 const router = express.Router()
 router.route('/sendemail').post(sendEmail)
@@ -16,6 +17,7 @@ router.route('/addleader').post(addLeader).get(getAllExco)
 router.route('/addproject').post(addProject).get(getProject)
 router.route('/editproject').post(editproject)
 router.route('/deleteproject').post(deleteProject)
-
+router.route('/addevent').post(addEvent).get(getEvent)
+router.route('/deleteevent').post(deleteEvent)
 
 export default router
