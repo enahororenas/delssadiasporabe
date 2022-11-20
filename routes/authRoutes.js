280c2f89db23551324d64578377525c126714deb
enahoroenas <orieroenahoro@yahoo.com>
2022-11-19 22:11:32 -0800

    added ann

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {register, login, updateUser,updateUsersImage,forgotPassword,valPassword,
-    addNewUserToDB,makeAUserAdmin,getBday} from '../controllers/authController.js'
+    addNewUserToDB,makeAUserAdmin,getBday,getAnn} from '../controllers/authController.js'
 import 'express-async-errors'
 import authenticateUser from '../middleware/auth.js'
 
@@ -13,6 +13,7 @@ router.route('/updateImage').post(updateUsersImage)
 router.route('/addnewuser').post(addNewUserToDB)
 router.route('/makeadmin').post(makeAUserAdmin)
 router.route('/getbday').get(getBday)
+router.route('/getann').get(getAnn)
 router.route('/forgotpassword').post(forgotPassword)
 router.route('/valpassword').post(valPassword)
 
