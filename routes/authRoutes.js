6d9c83c988983a8b48dca953dae683b4f8685bcb
enahoroenas <orieroenahoro@yahoo.com>
2022-12-27 02:16:54 -0800

    added wakeup

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import {register, login, updateUser,updateUsersImage,forgotPassword,valPassword,
-    addNewUserToDB,makeAUserAdmin,getBday,getAnn} from '../controllers/authController.js'
+    addNewUserToDB,makeAUserAdmin,getBday,getAnn,wakeUp} from '../controllers/authController.js'
 import 'express-async-errors'
 import authenticateUser from '../middleware/auth.js'
 
@@ -8,6 +8,7 @@ const router = express.Router()
 
 router.route('/register').post(register)
 router.route('/login').post(login)
+router.route('/wake').post(wakeUp)
 router.route('/forgotpassword').post(forgotPassword)
 router.route('/valpassword').post(valPassword)
 router.route('/updateUser').patch(authenticateUser,updateUser)
